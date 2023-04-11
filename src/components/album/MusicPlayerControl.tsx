ce4864f104514c436f350654b23e12f98d5353bb
Ariel Aquino <arielaquino.cs@gmail.com>
2023-04-11 03:23:51 -0700

    Add music player controls ui

--- src/components/album/MusicPlayerControl.tsx
+++ src/components/album/MusicPlayerControl.tsx
@@ -1,3 +1,69 @@
+import {
+  PlayCircle,
+  Repeat,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+} from '@phosphor-icons/react';
+import * as Progress from '@radix-ui/react-progress';
+import * as Slider from '@radix-ui/react-slider';
+import { useEffect, useState } from 'react';
+
 export const MusicPlayerControl: React.FC = () => {
-  return <div>Music Player Control</div>;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(50), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <div className='flex items-center justify-center space-x-5'>
+        <Shuffle
+          className='duration-200 text-slate-400 hover:text-slate-200'
+          size={24}
+          weight='fill'
+        />
+        <SkipBack
+          className='duration-200 text-slate-400 hover:text-slate-200'
+          size={24}
+          weight='fill'
+        />
+        <PlayCircle
+          className='duration-200 text-slate-200 hover:text-slate-100'
+          size={48}
+          weight='fill'
+        />
+        <SkipForward
+          className='duration-200 text-slate-400 hover:text-slate-200'
+          size={24}
+          weight='fill'
+        />
+        <Repeat
+          className='duration-200 text-slate-400 hover:text-slate-200'
+          size={24}
+          weight='fill'
+        />
+      </div>
+      <div className='flex items-center space-x-2'>
+        <span className='select-none'>1:30</span>
+        <form>
+          <Slider.Root
+            className='relative flex items-center h-2 select-none touch-none w-96'
+            defaultValue={[0]}
+            max={100}
+            step={1}
+            aria-label='Volume'
+          >
+            <Slider.Track className='relative flex-grow h-1 rounded-full bg-slate-700'>
+              <Slider.Range className='absolute h-full rounded-full bg-slate-300' />
+            </Slider.Track>
+            <Slider.Thumb className='block w-[20px] h-[20px] rounded-full bg-white shadow-md border-[10px] hover:bg-slate-100 duration-200 focus:outline-none focus:shadow-sm' />
+          </Slider.Root>
+        </form>
+        <span className='select-none'>3:00</span>
+      </div>
+    </div>
+  );
 };
