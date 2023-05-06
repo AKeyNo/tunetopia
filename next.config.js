3e3c5a41a92536bfc57d7a5784474a1f67157402
Ariel Angelo Aquino <arielaquino.cs@gmail.com>
2023-05-05 23:24:53 -0700

    Add remote image handling for supabase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', ''),
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
