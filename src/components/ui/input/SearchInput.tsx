36894c8824eb88c189576ccb271286c0302bfc4c
Ariel Angelo Aquino <arielaquino.cs@gmail.com>
2023-06-01 03:24:09 -0700

    Add label to SearchInput

--- src/components/ui/input/SearchInput.tsx
+++ src/components/ui/input/SearchInput.tsx
@@ -1,14 +1,18 @@
 export const SearchInput: React.FC<{
   className?: string;
+  label?: string;
   onChange: React.Dispatch<React.SetStateAction<string>>;
   'data-cy'?: string;
-}> = ({ className, onChange, 'data-cy': dataCy }) => {
+}> = ({ className, onChange, 'data-cy': dataCy, label }) => {
   return (
-    <input
-      className={`w-64 rounded-full h-8 text-black px-2 ${className}`}
-      type='text'
-      onChange={(e) => onChange(e.target.value)}
-      data-cy={dataCy}
-    />
+    <div>
+      {label && <label className='block text-slate-300'>{label}</label>}
+      <input
+        className={`w-64 rounded-full h-8 text-black px-2 ${className}`}
+        type='text'
+        onChange={(e) => onChange(e.target.value)}
+        data-cy={dataCy}
+      />
+    </div>
   );
 };
